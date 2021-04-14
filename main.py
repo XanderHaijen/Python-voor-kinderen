ce65fbf19c989ac2350455f411f53172e2d0adf8
XanderHaijen <xander.haijen@student.kuleuven.be>
2021-04-14 18:08:21 +0200

    extra RockPaperScissorsFunc.py

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Schaar, steen, papier
+# 1. computer kiest
+# 2. jij kiest
+import random
+computer = random.choice(("schaar","steen","papier"))
+speler = input("Schaar, steen of papier? ")
+if computer == "schaar":
+    if speler == "schaar":
+        print("gelijkspel")
+    elif speler == "steen":
+        print("je bent gewonnen")
+    elif 
